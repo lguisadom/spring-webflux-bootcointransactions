9ace3dd0da4eac837c5c3e95b9a1798c6dc881d1
Luis Guisado <lguisadom@gmail.com>
2022-03-23 17:45:14 -0500

    BootCoin Transactions

---
+++ src/main/java/com/nttdata/lagm/bootcointx/SpringWebfluxBootCointTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.bootcointx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxBootCointTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxBootCointTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/lagm/bootcointx/SpringWebfluxBootCointTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.bootcointx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxBootCointTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
